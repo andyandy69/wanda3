1482ded39629c798dd5de0e93c2ef5b93b7a51f6
andyandy69 <71688600+andyandy69@users.noreply.github.com>
2021-01-11 23:03:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -59,13 +59,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 // 				}								
 				if message.Text == "我要看妞妞"{
 					t1 := time.NewTimer(1 * time.Second)
-// 					message = {
-// 					  "type": "image",
-// 					  "originalContentUrl": "https://i.imgur.com/z5yOT1e.jpg",
-// 					  "previewImageUrl": "https://i.imgur.com/z5yOT1e.jpg"
-// 					}
+					message = {
+					  "type": "image",
+					  "originalContentUrl": "https://i.imgur.com/z5yOT1e.jpg",
+					  "previewImageUrl": "https://i.imgur.com/z5yOT1e.jpg"
+					}
 					<- t1.C
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("汪!"+message)).Do(); err != nil {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message)).Do(); err != nil {
 					log.Print(err)
 					}
 				}
