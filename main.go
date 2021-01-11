c8344695d44fe3cbca2fb1131fceb6ff7e071fd4
andyandy69 <71688600+andyandy69@users.noreply.github.com>
2021-01-11 22:53:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -69,9 +69,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 // 					log.Print(err)
 // 					}
 					<- t1.C
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("汪!")).Do(); err != nil {
-					log.Print(err)
-					}
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("汪!")).Do();
 					<- t1.C
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("汪汪汪!!!")).Do(); err != nil {
 					log.Print(err)
