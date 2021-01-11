7b444fa87a702907f5ebf61e31180d853e111b3a
andyandy69 <71688600+andyandy69@users.noreply.github.com>
2021-01-11 23:44:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if message.Text == "我要看妞妞"{
 					t1 := time.NewTimer(3 * time.Second)
 					<- t1.C
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage("汪!"), linebot.NewImageMessage("https://i.imgur.com/z5yOT1e.jpg" , "https://i.imgur.com/z5yOT1e.jpg")).Do(); err != nil {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.TextMessage("汪!"), linebot.NewImageMessage("https://i.imgur.com/z5yOT1e.jpg" , "https://i.imgur.com/z5yOT1e.jpg")).Do(); err != nil {
 					log.Print(err)
 					}
 				}
